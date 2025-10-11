4edeecaf663b3a8a1cc37526d1638b9d36aadac7
sumit adep <adepsumit123@gmail.com>
2025-10-11 23:20:11 +0530

    wifi-init: int nvs-flash

--- app/main/app.c
+++ app/main/app.c
@@ -1,7 +1,26 @@
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+void wifi_init_sta();
+
 void app_main(void)
 {
+    wifi_init_sta();
 
     while (1)
     {
     }
+}
+
+void wifi_init_sta()
+{
+    char *tag = "WIFI-INIT";
+
+    ESP_LOGI(tag, "");
+
+    // Initialize NVS
+    // This is important to be done as wifi uses nvs to store phy layer parameter, user credentials, tx-power level etc,...
+    ESP_ERROR_CHECK(nvs_flash_init());
+
+    ESP_LOGI(tag, "...DONE");
 }
