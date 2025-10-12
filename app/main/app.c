802e60af9633e06cea0dc683b9f0682e517fc64a
sumit adep <83160210+sumitadep002@users.noreply.github.com>
2025-10-12 12:33:13 +0530

    Wifi: setup sta mode (#9)
    -cleanup app.c
    -wifi-init: int nvs-flash
    -init default event-loop and wifi-sta
    -add changes to use vs-code esp-idf extention
    -configure station mode to connct with specified ssid
    -define event handlers for wifi
    
    refer #8.

--- app/main/app.c
+++ app/main/app.c
@@ -1,75 +1,77 @@
-#include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/event_groups.h"
-#include "esp_wifi.h"
 #include "esp_log.h"
-#include "esp_event.h"
 #include "nvs_flash.h"
-#include "regex.h"
+#include "esp_netif.h"
+#include "esp_event.h"
+#include "esp_wifi.h"
 
-#define DEFAULT_SCAN_LIST_SIZE 1
-#define MIN_RSSI -30
-#define TAG "WIFI-SCANNING"
+#define TAG_MAIN "MAIN"
+#define TAG_WIFI "WIFI"
 
-static void setup_wifi();
-static void wifi_scanner();
+void wifi_init_sta();
+void event_handler(void *arg, esp_event_base_t event_base,
+                   int32_t event_id, void *event_data);
 
 void app_main(void)
 {
-    setup_wifi();
+    wifi_init_sta();
 
     while (1)
     {
-        wifi_scanner();
+        ESP_LOGI(TAG_MAIN, "Running....");
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
-void setup_wifi()
+void wifi_init_sta()
 {
+
+    ESP_LOGI(TAG_WIFI, "");
 
     // Initialize NVS
     // This is important to be done as wifi uses nvs to store phy layer parameter, user credentials, tx-power level etc,...
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
-    {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(nvs_flash_init());
 
-    // Initialize lightweight tcp/ip stack
-    // following task will create a thread to handle  network events such as incoming packets, timers, DHCP, ARP, etc
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
-    assert(sta_netif);
+    esp_netif_init();                    // Initialize TCP/IP stack
+    esp_event_loop_create_default();     // Create event loop
+    esp_netif_create_default_wifi_sta(); // Create default station interface, this will be studies later
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    esp_wifi_init(&cfg); // Initialize Wi-Fi with default config
 
-    wifi_ap_record_t ap_info[DEFAULT_SCAN_LIST_SIZE];
-    memset(ap_info, 0, sizeof(ap_info));
+    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL);
+    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL);
 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_start());
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+
+    esp_wifi_set_mode(WIFI_MODE_STA); // Set Wi-Fi to station mode
+    esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
+    esp_wifi_start();
+
+    ESP_LOGI(TAG_WIFI, "...DONE");
 }
 
-void wifi_scanner()
+// Event handler
+void event_handler(void *arg, esp_event_base_t event_base,
+                   int32_t event_id, void *event_data)
 {
-    esp_wifi_scan_start(NULL, true);
-
-    wifi_ap_record_t ap_info[DEFAULT_SCAN_LIST_SIZE];
-    memset(ap_info, 0, sizeof(ap_info));
-
-    uint16_t temp = DEFAULT_SCAN_LIST_SIZE;
-
-    // retrive device
-    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&temp, ap_info));
-
-    for (int i = 0; i < DEFAULT_SCAN_LIST_SIZE && ap_info[i].rssi > MIN_RSSI; i++)
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
     {
-        ESP_LOGI(TAG, "%s, %d", ap_info[i].ssid, ap_info[i].rssi);
+        esp_wifi_connect();
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        ESP_LOGI(TAG_WIFI, "Disconnected. Reconnecting...");
+        esp_wifi_connect();
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        ESP_LOGI(TAG_WIFI, "Got IP: " IPSTR, IP2STR(&event->ip_info.ip));
     }
 }
